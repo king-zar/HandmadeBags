aec25a71e87865ee0fccc2037f541b8a2930016e
king_x <kinga.zarska5@gmail.com>
2025-11-19 12:16:02 +0100

    add l9ightbox

--- script.js
+++ script.js
@@ -2,35 +2,65 @@
 const form = document.querySelector(".contact-form");
 if (form) {
   form.addEventListener("submit", function (e) {
-    e.preventDefault(); // blokuje domyślne wysłanie
+    e.preventDefault(); // blokuje domyslne wyslanie
 
     const email = document.getElementById("email").value.trim();
     const message = document.getElementById("message").value.trim();
 
-    // walidacja e-mail
     if (!email.includes("@")) {
       alert("Podaj poprawny adres e-mail!");
       return;
     }
 
-    // walidacja długości wiadomości
     if (message.length < 50) {
       alert("Wiadomość musi mieć przynajmniej 50 znaków!");
       return;
     }
 
-    // jeśli wszystko OK
+    // wszystko OK
     alert("Dziękujemy za wiadomość! Skontaktujemy się z Tobą wkrótce.");
-    form.reset(); // czyści formularz
+    form.reset(); // czysci formularz
   });
 }
 
+// aktualizacja licznika stron w czasie rzeczywistym
 const messageField = document.getElementById("message");
 const charCount = document.getElementById("charCount");
 
 if (messageField && charCount) {
   messageField.addEventListener("input", function () {
     const length = messageField.value.length;
-    charCount.textContent = `${length} / 50 znaków`;
+    charCount.textContent = "${length} / 50 znaków";
   });
 }
+
+// powiekszanie zdjecia
+const galleryImages = document.querySelectorAll(".gallery-grid img");
+
+galleryImages.forEach((img) => {
+  img.addEventListener("click", function () {
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+
+    // powiekszony obraz
+    const largeImg = document.createElement("img");
+    largeImg.src = this.src;
+
+    overlay.appendChild(largeImg);
+
+    // klikniecie w overlay zamyka powiekszenie
+    overlay.addEventListener("click", function () {
+      overlay.remove();
+    });
+
+    document.body.appendChild(overlay);
+
+    overlay.addEventListener("click", function () {
+      overlay.remove();
+    });
+
+    overlay.addEventListener("touchstart", function () {
+      overlay.remove();
+    });
+  });
+});
